d979ccb4d20275f0d2348bb3af4003feda5af22d
Simon Lindgren <simon.g.lindgren@gmail.com>
2017-07-03 11:23:35 +0200

    modified os command of sctl-minion to work :)

--- sctl-minion/os-cmd.go
+++ sctl-minion/os-cmd.go
@@ -3,11 +3,15 @@ package main
 import (
 	"bytes"
 	"os/exec"
+	"strings"
 )
 
 //RunCommand executes a command against the host shell
 func RunCommand(command string) (string, error) {
-	cmd := exec.Command(command)
+	parts := strings.Fields(command)
+	command = parts[0]
+	args := parts[1:len(parts)]
+	cmd := exec.Command(command, args...)
 	var out bytes.Buffer
 	cmd.Stdout = &out
 	var errOut bytes.Buffer
