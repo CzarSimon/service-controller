450cf1d7f3b658f951d21558bec79f9fc8fbcad1
Simon Lindgren <simon.g.lindgren@gmail.com>
2017-07-17 23:07:31 +0200

    Added configuring ufw to allow swarm connections

--- sctl/sctl-data/executables/linux/sctl-minion/setup-minion.sh
+++ sctl/sctl-data/executables/linux/sctl-minion/setup-minion.sh
@@ -1,14 +1,29 @@
-exec_folder="/usr/local/sbin/sctl-minion"
+setup_service() {
+  exec_folder="/usr/local/sbin/sctl-minion"
+  mv sctl-minion.service /etc/systemd/system/sctl-minion.service
 
-mv sctl-minion.service /etc/systemd/system/sctl-minion.service
+  mkdir $exec_folder
+  mv sctl-minion $exec_folder/sctl-minion
+  mv token-db $exec_folder/token-db
+}
 
-mkdir $exec_folder
-mv sctl-minion $exec_folder/sctl-minion
-mv token-db $exec_folder/token-db
+start_service() {
+  ufw allow 9105
+  systemctl enable sctl-minion.service
+  systemctl start sctl-minion.service
+}
 
-ufw allow 9105
+open_swarm_ports() {
+  ufw allow 2377/tcp
+  ufw allow 7946/tcp
+  ufw allow 7946/tcp
+  ufw allow 4789/udp
+}
 
-systemctl enable sctl-minion.service
-systemctl start sctl-minion.service
+# Setup + start tasks
+setup_service
+start_service
+open_swarm_ports
 
+# Removing setup-script
 rm setup-minion.sh
